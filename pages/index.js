307c41516bbbd55ef48018c40e5fe99ba40c49df
mogmarino <mogmarino@hotmail.com>
2022-08-16 07:39:15 -0300

    obtener datos de la DB

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
 import Head from "next/head";
 import Image from "next/image";
+import { PrismaClient } from "@prisma/client";
 
-export default function Home() {
+export default function Home({ categorias }) {
+  console.table(categorias);
   return <h1>Quiosco - App</h1>;
 }
+
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient();
+  const categorias = await prisma.categoria.findMany();
+  return {
+    props: { categorias },
+  };
+};
